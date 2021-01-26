d5397f50aa903c50ea89778588ded060c94d846e
LuisaFC <luisabsb@gmail.com>
2021-01-26 20:54:13 -0300

    aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import {Widget} from '../src/components/Widget/index'
+import Footer from '../src/components/Footer/index'
+import GithubCorner from '../src/components/GithubCorner/index'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of CSS</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da Galera</h1>
+
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
